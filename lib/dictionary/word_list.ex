525c7368a0e1c26cb2543b39ffc050eefe8651ab
Miguel Verissimo <m.verissimo@gmail.com>
2020-01-27 19:57:17 -0500

    Refactor word list to use an actor to hold the full list

--- lib/dictionary/word_list.ex
+++ lib/dictionary/word_list.ex
@@ -1,11 +1,17 @@
 defmodule Dictionary.WordList do
+  @me __MODULE__
+
+  def start_link() do
+    Agent.start_link(&word_list/0, name: @me)
+  end
+
   def word_list() do
     Path.expand("../../assets/words.txt", __DIR__)
     |> File.read!()
     |> String.split(~r/\n/)
   end
 
-  def get_random_word(word_list) do
-    word_list |> Enum.random()
+  def get_random_word() do
+    Agent.get(@me, &Enum.random/1)
   end
 end
--- test/dictionary_test.exs
+++ test/dictionary_test.exs
@@ -2,12 +2,7 @@ defmodule DictionaryTest do
   use ExUnit.Case
   doctest Dictionary
 
-  test "gets a word list" do
-    assert length(Dictionary.start()) > 0
-  end
-
-  test "gets a random word given a word list" do
-    word_list = ["foo", "bar", "baz", "qux"]
-    assert Dictionary.random_word(word_list) != ""
+  test "gets a random word" do
+    assert Dictionary.random_word() != ""
   end
 end
